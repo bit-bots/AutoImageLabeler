e4d07b2f63f7859f2ca7a0ae3a2c31a8e088d133
Jonas Hagge <5hagge@informatik.uni-hamburg.de>
2020-03-18 20:31:43 +0100

    different vector creation for goalpost and robot

--- fcn/keras/generatePredictions.py
+++ fcn/keras/generatePredictions.py
@@ -8,9 +8,9 @@ import math
 directory = "/srv/ssd_nvm/15hagge/testset/375/"
 imagelist = []
 
-annoType = "goalpost"
+annoType = "robot"
 model = mobilenet_unet(n_classes = 2)
-model.load_weights("./mobile.68")
+model.load_weights("nets/robots/mobile.69")
 
 for filename in Path(directory).rglob("*.png"):
     imagelist.append(filename)
@@ -26,7 +26,7 @@ for index, image in enumerate(imagelist):
     prediction = model.predict_segmentation(inp=img, out_fname="/tmp/stuff.png")
     prediction = cv2.imread("/tmp/stuff.png", cv2.IMREAD_GRAYSCALE)
 
-    # the following taken from https://docs.opencv.org/trunk/dd/d49/tutorial_py_contour_features.html (7.b)
+    # the following in part taken from https://docs.opencv.org/trunk/dd/d49/tutorial_py_contour_features.html (7.b)
     # everything below 155 seems to be background. 
     # 210 makes for better fitting bounding boxes on one test set. Might have to evaluate further
     ret, thresh = cv2.threshold(prediction, 210, 255, cv2.THRESH_BINARY)
@@ -37,14 +37,33 @@ for index, image in enumerate(imagelist):
         rect = cv2.minAreaRect(cnt)
         box = cv2.boxPoints(rect)
         box = np.int0(box)
+        # skip too small boxes. It is unlikely this is an actual goalpost
         if math.sqrt((box[0][0] - box[2][0]) ** 2 + (box[0][1] - box[2][1]) ** 2) < 50:
             continue
-        #print(math.sqrt((box[0][0] - box[2][0]) ** 2 + (box[0][1] - box[2][1]) ** 2))
-        #cv2.drawContours(prediction, [box], 0, (0, 0, 255), 2)
-        vector = f"""{{"x1": {box[0][0]}, "y1": {box[0][1]}, "x2": {box[1][0]}, "y2": {box[1][1]},"""\
-                 f""""x3": {box[2][0]}, "y3": {box[2][1]}, "x4": {box[3][0]}, "y4": {box[3][1]}}}"""
+        # cv2.drawContours(prediction, [box], 0, (0, 0, 255), 2)
+        # 4 coordinates to describe object
+        if annoType == "goalpost":
+            vector = f"""{{"x1": {box[0][0]}, "y1": {box[0][1]}, "x2": {box[1][0]}, "y2": {box[1][1]},"""\
+                     f""""x3": {box[2][0]}, "y3": {box[2][1]}, "x4": {box[3][0]}, "y4": {box[3][1]}}}"""
+        # bounding box
+        elif annoType == "robot":
+            minx = 100000
+            miny = 100000
+            maxx = 0
+            maxy = 0
+            # get min / max values for both axis to determine bounding box
+            for coordinate in vector:
+                if coordinate[0] < minx:
+                    minx = coordinate[0]
+                if coordinate[0] > maxx:
+                    maxx = coordinate[0]
+                if coordinate[1] < miny:
+                    miny = coordinate[1]
+                if coordinate[1] > maxy:
+                    maxy = coordinate[1]
+            vector = f"""{{"x1": {minx}, "y1": {miny}, "x2": {maxx}, "y2": {maxy}}}"""
         vectorlist.append(vector)
-        #print(vector)
+        # print(vector)
 
     #cv2.imshow("foo", prediction)
     #cv2.waitKey(0)
